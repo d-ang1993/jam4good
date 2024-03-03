123031059865035b3cedc65fd0196984a11119af
Daniel Ang <dan.justin.ang@gmail.com>
2024-03-03 11:43:43 -0500

    haa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+
+import logo from './utils/clown-pepe-the-frog.gif'
 import './App.css';
 
 function App() {
@@ -7,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Better Luck Next Time
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
